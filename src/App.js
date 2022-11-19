42e2d05c0fcbc0b076d96cda808ac899439b6c44
jyu-1 <jiabids@gmail.com>
2022-11-19 06:55:07 -0800

    initialize project with react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles/App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div className="app"></div>;
+    }
+}
+
+export default App;

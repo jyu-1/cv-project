04813ad330313c346d08a0cc26ce7da6c4da56d5
jyu-1 <jiabids@gmail.com>
2022-11-29 20:46:54 -0800

    update input handler

--- src/components/Main.js
+++ src/components/Main.js
@@ -47,36 +47,171 @@ class Main extends Component {
         };
     }
 
-    handleChange = (e) => {
+    handleInfoName = (e) => {
         this.setState({
             personalInfo: {
                 name: e.target.value,
+            },
+        });
+    };
+
+    handleInfoTitle = (e) => {
+        this.setState({
+            personalInfo: {
                 title: e.target.value,
+            },
+        });
+    };
+
+    handleInfoPhone = (e) => {
+        this.setState({
+            personalInfo: {
                 phone: e.target.value,
+            },
+        });
+    };
+
+    handleInfoEmail = (e) => {
+        this.setState({
+            personalInfo: {
                 email: e.target.value,
+            },
+        });
+    };
+
+    handleInfoCity = (e) => {
+        this.setState({
+            personalInfo: {
                 city: e.target.value,
             },
+        });
+    };
 
+    handleEduUniversity = (e) => {
+        this.setState({
             educationInfo: {
                 module: [
                     {
                         university: e.target.value,
+                    },
+                ],
+            },
+        });
+    };
+
+    handleEduCity = (e) => {
+        this.setState({
+            educationInfo: {
+                module: [
+                    {
                         city: e.target.value,
+                    },
+                ],
+            },
+        });
+    };
+
+    handleEduDegree = (e) => {
+        this.setState({
+            educationInfo: {
+                module: [
+                    {
                         degree: e.target.value,
+                    },
+                ],
+            },
+        });
+    };
+
+    handleEduStart = (e) => {
+        this.setState({
+            educationInfo: {
+                module: [
+                    {
                         start: e.target.value,
+                    },
+                ],
+            },
+        });
+    };
+
+    handleEduEnd = (e) => {
+        this.setState({
+            educationInfo: {
+                module: [
+                    {
                         end: e.target.value,
                     },
                 ],
             },
+        });
+    };
 
+    handleExpPosition = (e) => {
+        this.setState({
             experienceInfo: {
                 module: [
                     {
                         position: e.target.value,
+                    },
+                ],
+            },
+        });
+    };
+
+    handleExpCompany = (e) => {
+        this.setState({
+            experienceInfo: {
+                module: [
+                    {
                         company: e.target.value,
+                    },
+                ],
+            },
+        });
+    };
+
+    handleExpCity = (e) => {
+        this.setState({
+            experienceInfo: {
+                module: [
+                    {
                         city: e.target.value,
+                    },
+                ],
+            },
+        });
+    };
+
+    handleExpStart = (e) => {
+        this.setState({
+            experienceInfo: {
+                module: [
+                    {
                         start: e.target.value,
+                    },
+                ],
+            },
+        });
+    };
+
+    handleExpEnd = (e) => {
+        this.setState({
+            experienceInfo: {
+                module: [
+                    {
                         end: e.target.value,
+                    },
+                ],
+            },
+        });
+    };
+
+    handleExpDescription = (e) => {
+        this.setState({
+            experienceInfo: {
+                module: [
+                    {
                         description: e.target.value,
                     },
                 ],
@@ -88,9 +223,28 @@ class Main extends Component {
         return (
             <div className="main">
                 <div className="left">
-                    <Contact />
-                    <Education />
-                    <Experience />
+                    <Contact
+                        handleInfoName={this.handleInfoName}
+                        handleInfoTitle={this.handleInfoTitle}
+                        handleInfoPhone={this.handleInfoPhone}
+                        handleInfoEmail={this.handleInfoEmail}
+                        handleInfoCity={this.handleInfoCity}
+                    />
+                    <Education
+                        handleEduUniversity={this.handleEduUniversity}
+                        handleEduCity={this.handleEduCity}
+                        handleEduDegree={this.handleEduDegree}
+                        handleEduStart={this.handleEduStart}
+                        handleEduEnd={this.handleEduEnd}
+                    />
+                    <Experience
+                        handleExpPosition={this.handleExpPosition}
+                        handleExpCompany={this.handleExpCompany}
+                        handleExpCity={this.handleExpCity}
+                        handleExpStart={this.handleExpStart}
+                        handleExpEnd={this.handleExpEnd}
+                        handleExpDescription={this.handleExpDescription}
+                    />
                 </div>
                 <div className="right">
                     <Resume
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 
 class Resume extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         const { info, education, experience } = this.props;
 

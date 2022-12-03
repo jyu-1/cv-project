b3238ffc8803707134751c8274c4a75e988896fe
jyu-1 <jiabids@gmail.com>
2022-12-02 20:18:59 -0800

    change left side margins

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div className="app">
-                <div className="header">Resume Builder</div>
+                <div className="header">RESUME BUILDER</div>
                 <Main />
                 <div className="footer">
                     <span>
